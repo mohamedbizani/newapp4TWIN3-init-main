1817b3afb00c93147db24bb8f28b8ad183922e26
mohamed <mohamed.bizani@esprit.tn>
2024-12-19 23:32:57 +0100

    first

--- ordinateur/controller.js
+++ ordinateur/controller.js
@@ -1,15 +1,17 @@
 var express = require('express')
 var router = express.Router()
-const { list, create, update, deleteU, searchByPrice } = require('./ordinateurService')
-//var validate = require('../middleware/validation')
+const { list, create, update, deleteU ,searchByPriceRange} = require('./ordinateurService')
 
-const { socketHandler } = require('./ordinateur/controller');
-socketHandler(io); // Appel du gestionnaire d'événements Socket.IO
+
 
 router.get('/list', list)
 router.post('/create', create)
 router.put('/update/:id', update)
 router.delete('/delete/:id', deleteU)
-router.get('/recherche', searchByPrice);
+router.get('/search', searchByPriceRange)
+
+router.get('/search-category', (req, res) => {
+    res.render('categorie'); // Twig template `index.twig` sera rendu
+});
 
 module.exports = router
--- ordinateur/ordinateurModel.js
+++ ordinateur/ordinateurModel.js
@@ -1,9 +1,10 @@
 var mongoose = require('mongoose')
+const { boolean } = require('yup')
 var Schema = mongoose.Schema
 var Ordinateur = new Schema({
     modele : String,
     categorie : String,
-    dateFabrication  : Date,
+    date_fabrication : Date,
     prix : Number
 })
 module.exports = mongoose.model('ordinateurs', Ordinateur)
